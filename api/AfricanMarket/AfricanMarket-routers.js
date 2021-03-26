a4011f640395d2a1368c1be575009325f6be437a
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-03-26 12:29:04 -0600

    updated readme with correct endpoints and schema

--- api/AfricanMarket/AfricanMarket-routers.js
+++ api/AfricanMarket/AfricanMarket-routers.js
@@ -4,16 +4,24 @@ const jwt = require('jsonwebtoken')
 const market = require('./AfricanMarket-models')
 const router = express.Router()
 
-//owners and users should be able to get a list of items 
+//owners and users should be able to get a list of all items 
 router.get('/items', async (req, res, next) => {
 })
 
-//owner should be able to create a new item
-router.put('/items', async (req, res, next) => {
+//owners and users should be able to get an item by id
+router.get('/items/:id', async (req, res, next) => {
 })
 
-//owner should be able to update an existing item 
-router.put('/items', async (req, res, next) => {
+//owners and users should be able to get an item by a specific category 
+router.get('/items/:category', async (req, res, next) => {
+})
+
+//owner should be able to create a new item
+router.post('/items/addItem', async (req, res, next) => {
+})
+
+//owner should be able to edit/update an existing item 
+router.put('/items/:id', async (req, res, next) => {
 })
 
 //owner should be able to delete an item

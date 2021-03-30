b35cea78eae5c1027a89b1c6765c5a4a5191a1c4
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-03-29 18:37:29 -0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server.js')
 require("dotenv").config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)

46c5d1580d16c3960c5b2259c879cd69e4d2102b
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-03-31 18:31:39 -0600

    changes

--- api/middleware/users.js
+++ api/middleware/users.js
@@ -1,32 +1,32 @@
 const jwt = require("jsonwebtoken")
-const { JWT_SECRET } = require("../secrets");
+const { JWT_SECRET } = require("../secrets/index");
 
 const restrict = async (req, res, next) => {
-    try {
-      const token = req.headers.authorization
+  try {
+    const token = req.headers.authorization
 
-      if(!token) {
+    if (!token) {
+      return res.status(401).json({
+        message: "token required"
+      })
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
         return res.status(401).json({
-          message: "token required"
+          message: "invalid token"
         })
       }
 
-      jwt.verify(token, JWT_SECRET, (err, decoded) => {
-        if(err) {
-          return res.status(401).json({
-            message: "invalid token"
-          })
-        }
+      req.token = decoded
 
-        req.token = decoded
-
-        next()
-      })
-    } catch(err) {
-      next(err)
-    }
-  };
-
-  module.exports = {
-    restrict
+      next()
+    })
+  } catch (err) {
+    next(err)
   }
+};
+
+module.exports = {
+  restrict
+}
